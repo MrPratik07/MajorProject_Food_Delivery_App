76eeb0652f56a11cc94ea77e4014c72d520128a1
Pratik Gaikwad <pratikgaikwad551@gmail.com>
2021-06-09 13:57:03 +0530

    added offer card

--- src/components/offer/OfferPage.js
+++ src/components/offer/OfferPage.js
@@ -1,9 +1,98 @@
 import React from 'react';
-
+import '../offer/OfferPage.css'
+import {
+    FaHamburger, 
+  } from "react-icons/fa";
+  
 
 const OfferPage = () =>{
     return (
-        <div>This is Offer Page</div>
+    
+    <div className="offer-container">
+        
+        
+            <div className="offer-card">
+            <div className="food-image">
+                <FaHamburger id="food-item-logo"/>
+            </div>
+            <div className="food-content">
+            <div className="food-title">
+            <h3 id="food-name">Ham Burger</h3>
+            <h4 id="food-cost">Buy a Ham Burger and get 1 Extraa Ham Burger for free. Offer is valid for today</h4>
+            </div>
+            <div className="add-to-cart">
+                <h2>Use Code : B1G1</h2>
+            </div>
+            </div>
+            </div>
+        
+        
+        
+        
+            <div className="offer-card">
+            <div className="food-image">
+                <FaHamburger id="food-item-logo"/>
+            </div>
+            <div className="food-content">
+            <div className="food-title">
+            <h3 id="food-name">Ham Burger</h3>
+            <h4 id="food-cost">Buy a Ham Burger and get 1 Extraa Ham Burger for free. Offer is valid for today</h4>
+            </div>
+            <div className="add-to-cart">
+                <h2>Use Code : B1G1</h2>
+            </div>
+            </div>
+            </div>
+
+            <div className="offer-card">
+            <div className="food-image">
+                <FaHamburger id="food-item-logo"/>
+            </div>
+            <div className="food-content">
+            <div className="food-title">
+            <h3 id="food-name">Ham Burger</h3>
+            <h4 id="food-cost">Buy a Ham Burger and get 1 Extraa Ham Burger for free. Offer is valid for today</h4>
+            </div>
+            <div className="add-to-cart">
+                <h2>Use Code : B1G1</h2>
+            </div>
+            </div>
+            </div>
+
+            <div className="offer-card">
+            <div className="food-image">
+                <FaHamburger id="food-item-logo"/>
+            </div>
+            <div className="food-content">
+            <div className="food-title">
+            <h3 id="food-name">Ham Burger</h3>
+            <h4 id="food-cost">Buy a Ham Burger and get 1 Extraa Ham Burger for free. Offer is valid for today</h4>
+            </div>
+            <div className="add-to-cart">
+                <h2>Use Code : B1G1</h2>
+            </div>
+            </div>
+            </div>
+
+            <div className="offer-card">
+            <div className="food-image">
+                <FaHamburger id="food-item-logo"/>
+            </div>
+            <div className="food-content">
+            <div className="food-title">
+            <h3 id="food-name">Ham Burger</h3>
+            <h4 id="food-cost">Buy a Ham Burger and get 1 Extraa Ham Burger for free. Offer is valid for today</h4>
+            </div>
+            <div className="add-to-cart">
+                <h2>Use Code : B1G1</h2>
+            </div>
+            </div>
+            </div>
+        
+        
+    </div>
+        
+        
     )
 }
 
