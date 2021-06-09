7b51b97727130a1f11b37dcd49dd6e00863ee8c2
Pratik Gaikwad <pratikgaikwad551@gmail.com>
2021-06-09 23:07:44 +0530

    solved the issue of router

--- src/components/Controller.js
+++ src/components/Controller.js
@@ -4,15 +4,20 @@ import HomePage from './home/HomePage';
 import OfferPage from './offer/OfferPage';
 import HistoryPage from './history/HistoryPage';
 import CartPage from './cartpage/CartPage';
+import Navbar from '../components/header/Navbar';
+import Footer from '../components/footer/Footer';
+
 const Controller = () =>{
     return(
         <Router>
+        <Navbar/>
             <Switch>
                 <Route exact path="/" render={(props)=> <HomePage {...props}/>}/>
                 <Route path="/offers" render={(props)=> <OfferPage {...props}/>}/>
                 <Route path="/history" render={(props)=> <HistoryPage {...props}/>}/>
                 <Route path="/cart" render={(props)=> <CartPage {...props}/>}/>
             </Switch>
+        <Footer/>    
         </Router>
     )
 }
