38adc3ff7dc5dfbc0f3f736adefe55fb4c22f8c1
Pratik Gaikwad <pratikgaikwad551@gmail.com>
2021-06-10 13:28:57 +0530

    added order card

--- src/components/history/HistoryPage.js
+++ src/components/history/HistoryPage.js
@@ -1,9 +1,18 @@
 import React from 'react';
-
+import '../history/HistoryPage.css'
 const HistoryPage = () =>{
     return(
-        <div>
-            this is History Page
+        <div className="history-container">
+        <div className="order-container">
+        <div className="left-details">
+            <h3>Order Id : 1</h3>
+            <h4>Date : 10-06-2021</h4>
+        </div>
+        <div className="right-details">
+            <h3>Rs 210</h3>
+            <h4>Delivered</h4>
+        </div>
+        </div>
         </div>
     )
 }
