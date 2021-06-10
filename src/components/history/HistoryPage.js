5c86bfc1aa25e1596fcc9aca039c3971a80b95e4
Pratik Gaikwad <pratikgaikwad551@gmail.com>
2021-06-10 17:06:10 +0530

    css added to history page

--- src/components/history/HistoryPage.js
+++ src/components/history/HistoryPage.js
@@ -2,18 +2,35 @@ import React from 'react';
 import '../history/HistoryPage.css'
 const HistoryPage = () =>{
     return(
+        <>
+        <div className="history-header">
+        <h1>Your Order History</h1>
+        </div>
+        
         <div className="history-container">
         <div className="order-container">
         <div className="left-details">
-            <h3>Order Id : 1</h3>
-            <h4>Date : 10-06-2021</h4>
+            <h3 id="orderid">Order Id : 1</h3>
+            <h4 id="date">Date : 10-06-2021</h4>
         </div>
         <div className="right-details">
-            <h3>Rs 210</h3>
-            <h4>Delivered</h4>
+            <h3 id="money">Rs 210</h3>
+            <h4 id="order-deliver">Delivered</h4>
+        </div>
+        </div>
+
+        <div className="order-container">
+        <div className="left-details">
+            <h3 id="orderid">Order Id : 2</h3>
+            <h4 id="date">Date : 10-06-2021</h4>
+        </div>
+        <div className="right-details">
+            <h3 id="money">Rs 510</h3>
+            <h4 id="order-deliver">Delivered</h4>
         </div>
         </div>
         </div>
+       </> 
     )
 }
 
